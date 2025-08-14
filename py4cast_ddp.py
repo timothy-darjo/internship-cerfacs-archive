74e59a4e334404e6d754933c1b9fd5c7cead39e4
Timothy DARJO <timothy.darjo@meteo.fr>
2025-08-14 18:30:14 +0200

    Tweaked script for arpege integration

--- py4cast_ddp.py
+++ py4cast_ddp.py
@@ -1,27 +1,28 @@
 from typing import Any
 from py4cast.datasets import get_datasets
 import torch
-from torch.utils.data import DataLoader
 import os
 
 #loading the dataset - code from oscar
 
+SAVE_EVERY_EPOCH = True
+
 dataset_configuration: dict[str, Any] = {
     "periods": {
       "train": {
-        "start": 20200101,
-        "end": 20221231,
+        "start": 20230101,
+        "end": 20230101,
         "obs_step": 3600,
       },
       "valid": {
-        "start": 20230101,
-        "end": 20231231,
+        "start": 20230102,
+        "end": 20230102,
         "obs_step": 3600,
         "obs_step_btw_t0": 10800,
       },
       "test": {
-        "start": 20240101,
-        "end": 20240831,
+        "start": 20230103,
+        "end": 20230103,
         "obs_step": 3600,
         "obs_step_btw_t0": 10800,
       },
@@ -40,36 +41,52 @@ dataset_configuration: dict[str, Any] = {
     "params": {
       "aro_r2": {
         "levels": [2],
-        "kind": "input_output",
+        "kind": "output",
         },
       "aro_tp": {
         "levels": [0],
-        "kind": "input_output",
+        "kind": "output",
         },
       "aro_u10": {
         "levels": [10],
-        "kind": "input_output",
+        "kind": "output",
         },
       "aro_v10": {
         "levels": [10],
-        "kind": "input_output",
+        "kind": "output",
         },
       "aro_t": {
         "levels": [250, 500, 700, 850],
-        "kind": "input_output",
+        "kind": "output",
         },
       "aro_u": {
         "levels": [250, 500, 700, 850],
-        "kind": "input_output",
+        "kind": "output",
         },
       "aro_v": {
         "levels": [250, 500, 700, 850],
-        "kind": "input_output",
+        "kind": "output",
         },
       "aro_z": {
         "levels": [250, 500, 700, 850],
-        "kind": "input_output",
+        "kind": "output",
         },
+      "arp_t": {
+        "levels": [250, 500, 700, 850],
+        "kind": "input",
+      },
+      "arp_u":{
+        "levels": [250, 500, 700, 850],
+        "kind": "input",
+      },
+      "arp_v":{
+        "levels": [250, 500, 700, 850],
+        "kind": "input",
+      },
+      "arp_z":{
+        "levels": [250, 500, 700, 850],
+        "kind": "input",
+      },
     },
 }
 
@@ -86,50 +103,6 @@ train, test, val = get_datasets(
 print(train)
 print(type(train))
 
-# choose workers based on CPU count
-num_workers = min(4, os.cpu_count())
-batch_size = 32 # adjust for GPU/CPU memory
-
-def collate_items(batch):
-    # batch is a list of Item objects
-    inputs = [item.inputs.tensor.squeeze(dim=0) for item in batch]
-    targets = [item.outputs.tensor.squeeze(dim=0) for item in batch]
-    
-    # Stack into tensors for batching
-    inputs = torch.stack(inputs, dim=0)
-    targets = torch.stack(targets, dim=0)
-    
-    return inputs, targets
-
-train_loader = DataLoader(
-    train,
-    batch_size=batch_size,            
-    shuffle=False,
-    num_workers=num_workers,
-    pin_memory=True,
-    persistent_workers=True,   # keeps workers alive between epochs
-    collate_fn=collate_items
-)
-
-val_loader = DataLoader(
-    val,
-    batch_size=batch_size,
-    shuffle=False,
-    num_workers=num_workers,
-    pin_memory=True,
-    persistent_workers=True,
-    collate_fn=collate_items
-)
-
-test_loader = DataLoader(
-    test,
-    batch_size=batch_size,
-    shuffle=False,
-    num_workers=num_workers,
-    pin_memory=True,
-    persistent_workers=True,
-    collate_fn=collate_items
-)
 
 #loading the model
 
@@ -158,12 +131,15 @@ model = GaussianDiffusion(
 
 loss_fn = torch.nn.MSELoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+epoch_count = 1
 
-print("cuda available? ",torch.cuda.is_available())
-
-for input, target in train_loader:
+for item in train:
+    input, target = item.forcing.tensor, item.outputs.tensor
     input_tensor = input.permute(0,3,1,2) #reshape to fit format
     target_tensor = target.permute(0,3,1,2)
+    #resizing input: not necessary?
+    print(input_tensor.shape,target_tensor.shape,"shapes")
+
     # Maintenant tu peux appeler ton modèle avec input pour générer et target pour avoir une loss
 
     # Zero your gradients for every batch!
@@ -176,3 +152,4 @@ for input, target in train_loader:
 
     # Adjust learning weights
     optimizer.step()
+torch.save(model.state_dict(), "model_weights.pth")
